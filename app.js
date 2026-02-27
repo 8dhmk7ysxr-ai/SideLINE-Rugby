a8c484b4bfe0233dae06d72070c50848e71dae4b
8dhmk7ysxr-ai <hendrik.malan80@gmail.com>
2026-02-27 17:03:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let match = {
+let match = JSON.parse(localStorage.getItem("sidelineMatch")) || {
     gainlinePlus: 0,
     gainlineMinus: 0,
     domTackPlus: 0,
@@ -7,11 +7,21 @@ let match = {
     quickBallMinus: 0,
     turnoverWon: 0,
     turnoverLost: 0,
-    penalties: 0
+    penalties: 0,
+    scrumWon: 0,
+    scrumLost: 0,
+    tightheadWon: 0,
+    lineoutWon: 0,
+    lineoutLost: 0
 };
+
+function saveData() {
+    localStorage.setItem("sidelineMatch", JSON.stringify(match));
+}
 
 function updateStat(stat) {
     match[stat]++;
+    saveData();
     displayStats();
 }
 
@@ -21,32 +31,35 @@ function percent(a, b) {
 }
 
 function displayStats() {
+
     let gainTotal = match.gainlinePlus + match.gainlineMinus;
     let tackleTotal = match.domTackPlus + match.domTackMinus;
     let quickTotal = match.quickBallPlus + match.quickBallMinus;
+    let scrumTotal = match.scrumWon + match.scrumLost;
+    let lineoutTotal = match.lineoutWon + match.lineoutLost;
 
     document.getElementById("statsOutput").innerHTML =
+        "<strong>Collision</strong><br>" +
         "Gainline: " + percent(match.gainlinePlus, match.gainlineMinus) + "% (" + match.gainlinePlus + "/" + gainTotal + ")<br>" +
-        "Tackle Dominance: " + percent(match.domTackPlus, match.domTackMinus) + "% (" + match.domTackPlus + "/" + tackleTotal + ")<br>" +
-        "Quick Ball: " + percent(match.quickBallPlus, match.quickBallMinus) + "% (" + match.quickBallPlus + "/" + quickTotal + ")<br>" +
+        "Tackle Dom: " + percent(match.domTackPlus, match.domTackMinus) + "% (" + match.domTackPlus + "/" + tackleTotal + ")<br><br>" +
+
+        "<strong>Breakdown</strong><br>" +
+        "Quick Ball: " + percent(match.quickBallPlus, match.quickBallMinus) + "% (" + match.quickBallPlus + "/" + quickTotal + ")<br><br>" +
+
+        "<strong>Set Piece</strong><br>" +
+        "Scrums: " + percent(match.scrumWon, match.scrumLost) + "% (" + match.scrumWon + "/" + scrumTotal + ")<br>" +
+        "Tightheads Won: " + match.tightheadWon + "<br>" +
+        "Lineouts: " + percent(match.lineoutWon, match.lineoutLost) + "% (" + match.lineoutWon + "/" + lineoutTotal + ")<br><br>" +
+
+        "<strong>Discipline</strong><br>" +
         "Turnovers Won: " + match.turnoverWon + "<br>" +
         "Turnovers Lost: " + match.turnoverLost + "<br>" +
         "Penalties: " + match.penalties;
 }
 
 function resetMatch() {
-    match = {
-        gainlinePlus: 0,
-        gainlineMinus: 0,
-        domTackPlus: 0,
-        domTackMinus: 0,
-        quickBallPlus: 0,
-        quickBallMinus: 0,
-        turnoverWon: 0,
-        turnoverLost: 0,
-        penalties: 0
-    };
-    displayStats();
+    localStorage.removeItem("sidelineMatch");
+    location.reload();
 }
 
 displayStats();
